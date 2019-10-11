702516a83b36b074e3cb7a42ae51b7abcdaed114
ruben <rubenmontes330@gmail.com>
2019-10-11 09:51:38 +0100

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
+  STRIPE_KEY: "pk_test_rYK7bCCGeDKh1QhG2zhkMRcj00W4hwHxgT",
   MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: "us-east-1",
--- src/containers/NotFound.js
+++ src/containers/NotFound.js
@@ -4,7 +4,7 @@ import "./NotFound.css";
 export default function NotFound() {
   return (
     <div className="NotFound">
-       <p >404</p>
+       <p className="E404">404</p>
       <h3>Sorry, page not found!</h3>
     </div>
   );
